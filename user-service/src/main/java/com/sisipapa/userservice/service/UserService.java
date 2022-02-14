425b8807ec564289a87a95c5de2d9a53f99a6ad2
sisipapa <sisiapap239@gmail.com>
2022-02-14 23:15:44 +0900

    1.Users Microservice - AuthenticationFilter추가 2.Users Microservice - loadUserByUsername() 구현 3.Users Microservice - Routes 정보 변경 4.Users Microservice - Routes 테스트

--- user-service/src/main/java/com/sisipapa/userservice/service/UserService.java
+++ user-service/src/main/java/com/sisipapa/userservice/service/UserService.java
@@ -2,8 +2,9 @@ package com.sisipapa.userservice.service;
 
 import com.sisipapa.userservice.dto.UserDto;
 import com.sisipapa.userservice.jpa.UserEntity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
     UserDto createUser(UserDto userDto);
     UserDto getUserByUserId(String userId);
     Iterable<UserEntity> getUserByAll();
